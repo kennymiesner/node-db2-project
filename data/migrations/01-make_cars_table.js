4b5f687aade432990896eaa9836cc99731270f8e
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-07 17:46:37 -0500

    Add car schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,12 +1,12 @@
 exports.up = async function (knex) {
   await knex.schema.createTable('cars', table => {
-    table.increments().notNullable()
-    table.text('vin', 17).unique().notNullable()
-    table.text('make', 128).notNullable()
-    table.text('model', 128).notNullable()
+    table.increments()
+    table.string('vin', 17).unique().notNullable()
+    table.string('make', 128).notNullable()
+    table.string('model', 128).notNullable()
     table.integer('mileage').unsigned().notNullable()
-    table.text('title', 128)
-    table.text('transmission', 128)
+    table.string('title', 128)
+    table.string('transmission', 128)
   })
 }
 
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,36 +1,36 @@
-// STRETCH
-const cars = [
-  {
-    vin: '12345678901234567',
-    make: 'toyota',
-    model: 'prius',
-    mileage: 215000,
-    title: 'clean',
-    transmission: 'manual',
-  },
-  {
-    vin: '23456789012345678',
-    make: 'toyota',
-    model: 'corolla',
-    mileage: 115000,
-    title: 'salvage',
-  },
-  {
-    vin: '34567890123456789',
-    make: 'ford',
-    model: 'focus',
-    mileage: 15000,
-  },
-]
+// // STRETCH
+// const cars = [
+//   {
+//     vin: '12345678901234567',
+//     make: 'toyota',
+//     model: 'prius',
+//     mileage: 215000,
+//     title: 'clean',
+//     transmission: 'manual',
+//   },
+//   {
+//     vin: '23456789012345678',
+//     make: 'toyota',
+//     model: 'corolla',
+//     mileage: 115000,
+//     title: 'salvage',
+//   },
+//   {
+//     vin: '34567890123456789',
+//     make: 'ford',
+//     model: 'focus',
+//     mileage: 15000,
+//   },
+// ]
 
-// exports.seed = function (knex) {
-//   return knex('cars')
-//     .truncate().then(() => {
-//       return knex('cars').insert(cars)
-//     })
+// // exports.seed = function (knex) {
+// //   return knex('cars')
+// //     .truncate().then(() => {
+// //       return knex('cars').insert(cars)
+// //     })
+// // }
+
+// exports.seed = async function (knex) {
+//   await knex('cars').truncate()
+//   await knex('cars').insert(cars)
 // }
-
-exports.seed = async function (knex) {
-  await knex('cars').truncate()
-  await knex('cars').insert(cars)
-}
